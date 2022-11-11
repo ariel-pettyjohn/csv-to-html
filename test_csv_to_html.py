af55b95902267ed3d6d824cb59d946bd11117b86
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-11-10 18:11:06 -0800

    Clean up names

--- test_csv_to_html.py
+++ test_csv_to_html.py
@@ -3,9 +3,9 @@ import csv_to_html
 
 class TestCSVToHTML(unittest.TestCase):
     def test_read_csv(self):
-        dictionary_list1 = list(csv_to_html.read_csv('test.csv'))
-        
-        self.assertEqual(dictionary_list1[0], {
+        dictionary_list = list(csv_to_html.read_csv('test.csv'))
+
+        self.assertEqual(dictionary_list[0], {
             '1' : '1',
             '2' : 'Eldon Base for stackable storage shelf, platinum',
             '3' : 'Muhammed MacIntyre',
