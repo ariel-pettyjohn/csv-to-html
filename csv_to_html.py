8b7ace37238ebb5cb95acfb057ff055b82f4deca
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-11-16 02:13:47 -0800

    Refactor deduplication logic using list comprehesions and improve naming

--- csv_to_html.py
+++ csv_to_html.py
@@ -19,25 +19,24 @@ def parse_csv_as_dictionary(path):
             print('CSV file must contain at least one row excluding headers')
 
 def parse_csv_as_html_string(path):
-    dictionary_list = list(parse_csv_as_dictionary(path))
-    headers = []
-   
-    for dictionary in dictionary_list:
-        for key in dictionary.keys():
-            if key not in headers:
-                headers.append(key)
-
-    return ''.join([
+    records = list(parse_csv_as_dictionary(path))
+    record_headers = [record.keys() for record in records]
+    csv_headers = list(dict.fromkeys([header for record in record_headers for header in record]))
+    
+    html_list = [
         '<table>', 
             *[
                 '<tr>', 
-                    *[f'<th>{escape(header)}</th>' for header in headers], 
+                    *[f'<th>{escape(header)}</th>' for header in csv_headers], 
                 '</tr>'
             ], 
             *[markup for row in [[
                 '<tr>', 
-                    *[f'<td>{escape(dictionary.get(header, ""))}</td>' for header in headers], 
+                    *[f'<td>{escape(record.get(header, ""))}</td>' for header in csv_headers], 
                 '</tr>'
-            ] for dictionary in dictionary_list] for markup in row], 
+            ] for record in records] for markup in row], 
         '</table>'
-    ])
+    ]
+    
+    html_string = ''.join(html_list)
+    return html_string
