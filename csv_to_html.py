e5deb57ce22cdc0c117d149dfe6d29870d325e24
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-11-16 02:34:02 -0800

    Refactor to make table structure more explicit and reduce line lengths

--- csv_to_html.py
+++ csv_to_html.py
@@ -1,15 +1,16 @@
 import csv
 from html import escape
 
+
 def parse_csv_as_dictionary(path):
     with open(path, newline='') as f:
         csv_reader = csv.reader(f)
 
         try:
             headers = next(csv_reader)
-        except: 
+        except:
             print('CSV file cannot be empty')
-        
+
         rows = list(csv_reader)
         try:
             if rows[0]:
@@ -18,30 +19,42 @@ def parse_csv_as_dictionary(path):
         except:
             print('CSV file must contain at least one row excluding headers')
 
+
 def parse_csv_as_html_string(path):
     records = list(parse_csv_as_dictionary(path))
     record_headers = [record.keys() for record in records]
-    csv_headers = list(dict.fromkeys([header for record in record_headers for header in record]))
-    
-    html_list = [
-        '<table>', 
+    csv_headers = list(dict.fromkeys(
+        [header for record in record_headers for header in record]
+    ))
+
+    header_cells = [f'<th>{escape(header)}</th>' for header in csv_headers]
+
+    def body_cells(record):
+        def body_cell(record, header):
+            return escape(record.get(header, ""))
+        return [
+            f'<td>{body_cell(record, header)}</td>' for header in csv_headers
+        ]
+
+    table_header = ['<tr>', *header_cells, '</tr>']
+
+    table_body = [
+        markup for row in [
+            ['<tr>', *body_cells(record), '</tr>']for record in records
+        ] for markup in row
+    ]
+
+    table = [
+        '<table>',
             '<thead>',
-                *[
-                    '<tr>', 
-                        *[f'<th>{escape(header)}</th>' for header in csv_headers], 
-                    '</tr>'
-                ], 
+                *table_header,
             '</thead>',
-            
+
             '<tbody>',
-                *[markup for row in [[
-                    '<tr>', 
-                        *[f'<td>{escape(record.get(header, ""))}</td>' for header in csv_headers], 
-                    '</tr>'
-                ] for record in records] for markup in row], 
+                *table_body,
             '</tbody>',
         '</table>'
     ]
-    
-    html_string = ''.join(html_list)
+
+    html_string = ''.join(table)
     return html_string
